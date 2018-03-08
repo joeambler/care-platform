77a5ad68dd405374f875243232126ca5ddb9c4ea
Joe Ambler <joe.ambler@outlook.com>
2018-03-08 22:03:22 +0000

    add index

--- src/server.js
+++ src/server.js
@@ -29,13 +29,14 @@ swaggerDocument.host = baseURL;
 
 app.use('/spec.json', (req, res) => res.send(swaggerDocument));
 
+app.use('/spec.yaml', (req, res) => res.send("YAML document coming soon."));
 //TODO: Impliment YAML spec
 //app.use('/spec.yaml', (req, res) => res.send(yaml.dump_all(swaggerDocument)));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-//TODO: Homepage
-app.get('/', (req, res) => res.redirect('/api-docs'));
+//app.get('/', (req, res) => res.redirect('/api-docs'));
+app.get('/', (req, res) => sendHomepage(res));
 //END DOCS
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
@@ -73,3 +74,21 @@ console.log('View Docs at:\t\t' + url.resolve(activeProtocol + baseURL, "/api-do
 console.log('Download Spec (JSON):\t' + url.resolve(activeProtocol + baseURL, "/spec.json"));
 console.log('Download Spec (YAML):\t' + url.resolve(activeProtocol + baseURL, "/spec.yaml"));
 console.log('API endpoint:\t\t' + url.resolve(activeProtocol +baseURL,"/v0"));
+
+
+function sendHomepage(res) {
+    res.send("<h1>Care Platform</h1><h2>View Documentation and test calls</h2><a href ="
+        + "/api-docs"
+        + ">Click here</a><h2>OpenAPI Specification (JSON)</h2><a href ="
+        + "/spec.json"
+        + ">Click here</a></br>"
+        + url.resolve(activeProtocol +baseURL,"/spec.json")
+        + "<h2>OpenAPI Specification (YAML) </h2><a href ="
+        + "/spec.yaml"
+        + ">Click here</a></br>"
+        + url.resolve(activeProtocol +baseURL,"/spec.yaml")
+        + "<h2>Swagger Editor - Generate SDK using the downloaded specification </h2><a href ="
+        + "http://editor.swagger.io"
+        + ">Click here</a><h2>API Endpoint </h2>"
+        + url.resolve(activeProtocol +baseURL,"/v0"))
+}
