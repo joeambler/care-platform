91cc91b02e28b0bba950850838f52e7b5713d249
Joe Ambler <joe.ambler@outlook.com>
2018-03-09 14:28:52 +0000

    Allow download of YAML spec

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const app = require('express')();
 const userContoller = require('./api/controllers/users');
 const componentController = require('./api/controllers/components');
 const url = require('url');
+const fs = require('fs');
 
 module.exports = app; // for testing
 
@@ -20,18 +21,23 @@ const baseURL = process.env.NODE_ENV === 'production'?
     process.env.HOST
     : 'localhost:10010';
 
+const specPath = __dirname + '/api/swagger/swagger.yaml';
+
 //DOCS
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-const swaggerDocument = YAML.load(__dirname + '/api/swagger/swagger.yaml');
+const swaggerDocument = YAML.load(specPath);
 
 swaggerDocument.host = baseURL;
 
-app.use('/spec.json', (req, res) => res.send(swaggerDocument));
+app.use('/spec.json', (req, res) => res.json(swaggerDocument));
+app.use('/spec.yaml', (req, res) => {
+    fs.readFile(specPath, 'utf8', function(err, contents) {
+        res.type('text/plain');
+        res.send(contents.replace('localhost:10010', baseURL));
+    });
+});
 
-app.use('/spec.yaml', (req, res) => res.send("YAML document coming soon."));
-//TODO: Impliment YAML spec
-//app.use('/spec.yaml', (req, res) => res.send(yaml.dump_all(swaggerDocument)));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
@@ -78,13 +84,13 @@ console.log('API endpoint:\t\t' + url.resolve(activeProtocol +baseURL,"/v0"));
 function sendHomepage(res) {
     res.send("<h1>Care Platform</h1><h2>View Documentation</h2><a href ="
         + "/api-docs"
-        + ">Click here</a><h2>OpenAPI Specification (JSON)</h2><a href ="
+        + ">Click here</a><h2>OpenAPI Specification (JSON)</h2><a  href ="
         + "/spec.json"
-        + ">Click here</a></br>"
+        + " download>Click here</a></br>"
         + url.resolve(activeProtocol +baseURL,"/spec.json")
         + "<h2>OpenAPI Specification (YAML) </h2><a href ="
         + "/spec.yaml"
-        + ">Click here</a></br>"
+        + " download>Click here</a></br>"
         + url.resolve(activeProtocol +baseURL,"/spec.yaml")
         + "<h2>Swagger Editor - Generate SDK using the downloaded specification </h2><a href ="
         + "http://editor.swagger.io"
